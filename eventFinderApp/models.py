2c720c799f2f45e4f850fa07c08bbe1d356d72c7
Cassie <cassietanadi@hotmail.com>
2019-09-07 11:04:29 +0800

    done

--- eventFinderApp/models.py
+++ eventFinderApp/models.py
@@ -1,30 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 class Event(models.Model):
-   event_id = models.CharField(max_length=200)
-   title = models.CharField(max_length=200)
+   name = models.CharField(max_length=200)
    location = models.CharField(max_length=200)
    venue = models.CharField(max_length=200)
    start_time = models.DateTimeField('start time and date')
    end_time = models.DateTimeField('end time and date')
-   # host = models.CharField(max_length=200)
-   categories = models.ManyToManyField('Category', related_name= 'events')
-   attendees = models.ManyToManyField('Users', related_name= 'users')
-
+   host = models.ForeignKey(User, related_name = 'hosting_events', on_delete = models.CASCADE)
+   categories = models.ManyToManyField('Category', related_name = 'events')
+   attendees = models.ManyToManyField(User, related_name = 'attending_events')
+   
    def __str__(self):
-      return self.title
-
+       return self.name
 
 class Category(models.Model):
    name = models.CharField(max_length=50)
-   category_id = models.CharField(max_length=50)
-       
+   
    def __str__(self):
-      return self.name
-
-
-class Users(models.Model):
-   user_id = models.CharField(max_length=50)
-   name = models.CharField(max_length=50)
-
-   def __str__(self):
-      return self.name
+       return self.name
